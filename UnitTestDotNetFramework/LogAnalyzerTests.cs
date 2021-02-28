83236ebee0952a49368af67d671e07b53a6ba851
m.hatami <m.hatami@son.ir>
2021-02-28 16:44:51 +0330

    add MockRepository

--- UnitTestDotNetFramework/LogAnalyzerTests.cs
+++ UnitTestDotNetFramework/LogAnalyzerTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using Rhino.Mocks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,12 +15,29 @@ namespace UnitTestDotNetFramework
         [Test]
         public void Analyze_TooShortFileName_CallsWebService()
         {
-            ManualMockService mockService = new ManualMockService();
-            LogAnalyzer log = new LogAnalyzer(mockService);
+            var mockService = new ManualMockService();
+            var log = new LogAnalyzer(mockService);
             string tooShortFileName = "abc.ext";
             log.Analyze(tooShortFileName);
-            Assert.AreEqual("Filename too short:abc.ext",
-            mockService.LastError);
+            Assert.AreEqual("Filename too short:abc.ext", mockService.LastError);
+        }
+
+        [Test]
+        public void Analyze_TooShortFileName_ErrorLoggedToService()
+        {
+            var mocks = new MockRepository();
+            var simulatedService = mocks.StrictMock<IWebService>();
+
+            using (mocks.Record())
+            {
+                simulatedService.LogError("Filename too short:abc.ext");
+            }
+
+            LogAnalyzer log = new LogAnalyzer(simulatedService);
+            string tooShortFileName = "abc.ext";
+            log.Analyze(tooShortFileName);
+            mocks.Verify(simulatedService);
+            
         }
     }
 }
